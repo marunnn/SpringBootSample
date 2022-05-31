760fc3f991ab850aeab5824eab6b1e591f1a5cec
MaruN <shikaku.32.n@gmail.com>
2022-05-31 21:04:45 +0900

    [SpringDismantling]8.2.3Complete:select(1件)

--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain.user.service;
 
+import java.util.List;
+
 import com.example.demo.domain.user.model.MUser;
 
 public interface UserService {
@@ -8,5 +10,15 @@ public interface UserService {
 	 * ユーザー登録
 	 */
 	public void signup(MUser user);
+	
+	/*
+	 * ユーザー取得
+	 */
+	public List<MUser> getUsers();
+	
+	/*
+	 * ユーザー取得(1件)
+	 */
+	public MUser getUserOne(String userId);
 
 }
