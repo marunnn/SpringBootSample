1ccaaba4c33fd5c969c867a1f63ee7ad52857c6b
MaruN <shikaku.32.n@gmail.com>
2022-06-19 21:07:23 +0900

    [SpringDismantling]11.2.7Complete:CSRF対策

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.logoutSuccessUrl("/login?logout");
 		
 		// CSRF対策を無効に設定(一時的)
-		http.csrf().disable();
+//		http.csrf().disable();
 		
 	}
 	
