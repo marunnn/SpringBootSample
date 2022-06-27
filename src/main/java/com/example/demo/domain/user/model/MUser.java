4f2219f36481cb79aa19571963977b29a96ca526
MaruN <shikaku.32.n@gmail.com>
2022-06-27 21:35:35 +0900

    [SpringDismantling]PdfComplete:ファイルダウンロード

--- src/main/java/com/example/demo/domain/user/model/MUser.java
+++ src/main/java/com/example/demo/domain/user/model/MUser.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.user.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -27,5 +28,26 @@ public class MUser {
 	private Department department;
 	
 	private List<Salary> salaryList;
+	
+	/**
+	 * CSV文字列の作成
+	 * @return
+	 */
+    public String toCsv() {
+    	
+        String genderStr = null;
+        
+        if (gender == 1) {
+            genderStr = "男性";
+        } else {
+            genderStr = "女性";
+        }
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        String csv = userId + ", " + userName + ", " + sdf.format(birthday) + ", " + age + ", " + genderStr + "\r\n";
+        
+        return csv;
+        
+    }
 
 }
